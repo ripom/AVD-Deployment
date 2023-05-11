40ee7c29e6251b0bb1f6ae9e74126d04f937627a
Riccardo Pomato <riccardopomato@hotmail.com>
2023-05-11 19:52:03 +0100

    Added az image builder run command in Images powershell script

--- Images/prerequisite.ps1
+++ Images/prerequisite.ps1
@@ -19,4 +19,9 @@ galleryID=${galleryID//\"/}
 #az role assignment create --assignee $principalid --role "Image Template Contributor" --scope $galleryID
 az role assignment create --assignee $principalid --role "Contributor" --resource-group $rg
 az sig image-definition create --gallery-name $galleryname --os-type $osType -g $rg -p $publisher -f $offer -s $sku -i $imageDefinitionName --architecture x64 --hyper-v-generation V2 --os-state Generalized 
-az image builder create -n $imageTemplate -g $rg --identity $identityname --image-source $imageSource --shared-image-destinations $galleryname/$imageDefinitionName=$location 
+az image builder create -n $imageTemplate -g $rg --identity $identityname --image-source $imageSource --shared-image-destinations $galleryname/$imageDefinitionName=$location --defer
+az image builder customizer add -n $imageTemplate -g $rg --customizer-name myPwshScript --exit-codes 0 1 --inline-script "mkdir c:\buildActions" "echo Azure-Image-Builder-Was-Here \> c:\buildActions\Output.txt" --type powershell --defer
+az image builder customizer add -n $imageTemplate -g $rg --customizer-name winUpdate --type windows-update --search-criteria IsInstalled=0 --filters "exclude:\$_.Title -like \'*Preview*\'" "include:\$true" --update-limit 20 --defer
+az image builder customizer add -n $imageTemplate -g $rg --customizer-name restart --type windows-restart --restart-timeout 10m --defer
+az image builder update -n $imageTemplate -g $rg
+az image builder run -n $imageTemplate -g $rg --no-wait
